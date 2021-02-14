2fbfe5dec026b672e17e5e375b6789e404fa68e4
15166 <15166@LAPTOP-480HP8SG>
2021-02-14 10:07:09 -0500

    added code to print the even numbers

---
+++ Assignment 1/src/assignment/pkg1/Assignment1.java
@@ -0,0 +1,16 @@
+
+package assignment.pkg1;
+public class Assignment1 {
+    public static void main(String[] args) {
+        
+        for(int i =0; i <= 100; i++){ //loops through numbers 0-100
+            
+            if(i%2 == 0){ //check to see if number is even
+                
+                System.out.println(i); //print if satisfies condition
+            
+            }
+        }
+    }
+    
+}
